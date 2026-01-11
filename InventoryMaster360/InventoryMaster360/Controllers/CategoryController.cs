cbcbff4990e9de58aafbe3107bd7fa57b459c865
Umut Yurdugül <umtyrdql@gmail.com>
2026-01-11 23:05:22 +0300

    Readme updated. Hard delete added for categories. Also validation for categories is used for first time

--- InventoryMaster360/InventoryMaster360/Controllers/CategoryController.cs
+++ InventoryMaster360/InventoryMaster360/Controllers/CategoryController.cs
@@ -1,16 +1,54 @@
+using FluentValidation;
 using InventoryMaster360.Models;
 using InventoryMaster360.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace InventoryMaster360.Controllers
 {
     public class CategoryController : Controller
     {
         private readonly IGenericRepository<Category> _categoryRepository;
+        private readonly IValidator<Category> _validator;
 
-        public CategoryController(IGenericRepository<Category> categoryRepository)
+        
+        public CategoryController(
+            IGenericRepository<Category> categoryRepository,
+            IValidator<Category> validator) 
         {
             _categoryRepository = categoryRepository;
+            _validator = validator; 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _categoryRepository.GetByIdAsync(id.Value);
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+
+        }
+
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteComfirmed(int id)
+        {
+            var category = await _categoryRepository.GetByIdAsync(id);
+            if (category == null)
+                return NotFound();
+
+            _categoryRepository.Remove(category);
+            await _categoryRepository.SaveAsync();
+            return RedirectToAction("Index");
         }
 
         public async Task<IActionResult> Index()
@@ -25,13 +63,22 @@ namespace InventoryMaster360.Controllers
             return View();
         }
 
+
+
         [HttpPost]
         public async Task<IActionResult> Create(Category category)
         {
-            if (!ModelState.IsValid)
+            FluentValidation.Results.ValidationResult result = await _validator.ValidateAsync(category);
+
+            if (!result.IsValid)
             {
-                return View(category);
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                }
+                return View(category); 
             }
+
             category.CreatedTime = DateTime.Now;
             category.UpdatedTime = DateTime.Now;
             await _categoryRepository.AddAsync(category);
